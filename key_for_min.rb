b219c0f2e29a8fbb71a2db8e858c0fc237ce2d41
Vincente Martinez <vinny@vinnymartinez.com>
2016-11-05 17:50:31 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.length >= 1
+    placeholder = "start"
+    key_holder = "valz"
+    name_hash.each do |keyz, valuez|
+      if placeholder == "start"
+        placeholder = valuez
+        key_holder = keyz
+      elsif placeholder >= valuez
+        placeholder = valuez
+        key_holder = keyz
+      end
+    end
+    key_holder
+  end
 end
